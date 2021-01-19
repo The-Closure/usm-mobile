f1ca8210c875f48c3a8d57eb8e81329c38370c09
ruba-salhani <ruba.salhani@gmail.com>
2021-01-19 18:33:54 +0300

    Community Posts Controllers, Repo, PostModel and CommentModel

---
+++ lib/Controllers/PostController.dart
@@ -0,0 +1,13 @@
+import 'package:get/get.dart';
+import 'package:usm_mobile/Models/PostModel.dart';
+import 'package:usm_mobile/Repositories/PostRepo.dart';
+
+class PostController extends GetxController {
+  List<Post> postsList = [];
+  PostRepo postRepo = Get.put(PostRepo(), tag: 'postRepo');
+
+  getPosts() async {
+    postsList = await postRepo.getPosts();
+    update();
+  }
+}
